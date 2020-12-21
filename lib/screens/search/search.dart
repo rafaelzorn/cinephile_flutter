4f507d5d6dd884ca7ff57904f97073e8cff35114
rafaelzorn <rafael.zorn@gmail.com>
2020-12-21 10:57:10 -0300

    Screen search

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -2,15 +2,50 @@ import 'package:flutter/material.dart';
 
 // Cf
 import 'package:cinephile_flutter/resources/strings.dart';
+import 'package:cinephile_flutter/resources/genres.dart';
+import 'package:cinephile_flutter/resources/colors.dart';
+import 'package:cinephile_flutter/screens/search/widgets/input-search.dart';
 
 class SearchScreen extends StatelessWidget {
+
+  Widget _renderListGenres() {
+    return ListView.builder(
+      itemCount: CfGenres.GENRES.length,
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.only(top: 15, bottom: 15),
+            child: Center(              
+              child: Text(
+                CfGenres.GENRES[index]['name'], 
+                style: TextStyle(fontSize: 18, color: CfColors.DARK_BLUE)
+              ),
+            ),          
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _renderContent() {
+    return Column(
+      children: <Widget>[
+        InputSearchWidget(),
+        Expanded(
+          child: _renderListGenres(),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(CfStrings.SEARCH),
       ),
-      body: Container(),
+      body: _renderContent(),
     );
   }
 }
---
+++ lib/screens/search/widgets/input-search.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class InputSearchWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
